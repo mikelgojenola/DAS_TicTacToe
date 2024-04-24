509e3a34cc60cde262e9e7de92203a910a064d91
mikelp <mikel.pedrolinaje@gmail.com>
2024-04-24 18:20:51 +0200

    Primer commit directorios, android studio

---
+++ app/src/main/java/com/das/tresenjuerga/MainActivity.java
@@ -0,0 +1,15 @@
+package com.das.tresenjuerga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //prueba de commit push y pull (mikelpe)
+    }
+}
